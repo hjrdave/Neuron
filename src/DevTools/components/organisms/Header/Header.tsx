201bfe60b0c68fa4a53abb332a7cebfb50cac763
davidsanders <dsanders_m633@outlook.com>
2023-11-28 14:59:46 -0500

    added logo neuron

--- src/DevTools/components/organisms/Header/Header.tsx
+++ src/DevTools/components/organisms/Header/Header.tsx
@@ -4,12 +4,20 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import MainNav from "../../molecules/MainNav";
 import PanelPosition from "../../atoms/PanelPosition";
+import NeuronLogo from "../../../assets/logo-neuron.webp";
 
 export default function Header() {
   return (
     <Row className={"d-flex justify-content-between border-bottom"}>
       <Col className={"px-0"}>
-        <MainNav />
+        <div className={"d-flex"}>
+          <div className={"py-2 px-3"}>
+            <Image src={NeuronLogo} width={30} />
+          </div>
+          <div className={"d-flex align-items-end"}>
+            <MainNav />
+          </div>
+        </div>
       </Col>
       <Col sm={4} className={"d-none d-sm-block"}>
         <PanelPosition />
