dbd6e9bd389dc169e99d888d1fc8c283257a9221
davidsanders <dsanders_m633@outlook.com>
2024-01-04 16:02:35 -0500

    updated npm publish with vanilla-neuron package

--- packages/vanilla-neuron/vite.config.ts
+++ packages/vanilla-neuron/vite.config.ts
@@ -1,5 +1,7 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -56,5 +58,5 @@ export default defineConfig({
       ],
     },
   },
-  plugins: [tsconfigPaths()],
+  plugins: [dts({ rollupTypes: true }), tsconfigPaths()],
 });
