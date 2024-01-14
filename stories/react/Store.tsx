cf4dd277126880b2f9538674d379ddbc10692291
davidsanders <dsanders_m633@outlook.com>
2024-01-14 08:46:01 -0500

    added storybook

---
+++ stories/react/Store.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Neuron from "../../package/react";
+
+interface IState {
+  fruit: string;
+  carList: string[];
+  isLoading: boolean;
+  person: {
+    name: string;
+    gender: string;
+    jobTitle: string;
+  };
+  score: number;
+  userName: string;
+}
+
+interface Person {
+  name: string;
+  gender: string;
+  jobTitle: string;
+}
+
+export interface ScoreActions {
+  increment: () => void;
+  decrement: () => void;
+}
+
+export const { State, Module, bridge, useNeuron } = Neuron.Store<IState>();
+
+export default function Store() {
+  return (
+    <>
+      <State<string> name={"fruit"} state={"apple"} />
+
+      <State<boolean> name={"isLoading"} state={false} />
+      <State<Person>
+        name={"person"}
+        state={{
+          name: "Bob",
+          gender: "male",
+          jobTitle: "Developer",
+        }}
+      />
+      {/**Persisted State */}
+      <State<number, ScoreActions>
+        name={"score"}
+        state={1000}
+        actions={(dispatch) => ({
+          increment: () =>
+            dispatch((payload) => {
+              payload.state = payload.prevState + 10;
+            }),
+          decrement: () =>
+            dispatch((payload) => {
+              payload.state = payload.prevState - 10;
+            }),
+        })}
+      />
+      <State<string> name={"userName"} state={"Captain Foo"} />
+      <State<string[]> name={"carList"} state={["toyota", "ford", "chevy"]} />
+    </>
+  );
+}
---
+++ stories/vanilla/vanilla.stories.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { StoryFn, Meta } from "@storybook/react";
+import Neuron from "../../package/vanilla";
+
+const store = Neuron.Store();
+store.add({
+  key: "foo",
+  state: "foo bar choo",
+});
+
+export default {
+  title: "Test NPM Package/vanilla",
+} as Meta;
+
+const VanillaTemplate: StoryFn = () => {
+  const [foo, setFoo] = useState(store.get("foo"));
+  useEffect(() => {
+    store.onDispatch((payload) => {
+      if (payload.key === "foo") {
+        setFoo(payload.state as any);
+      }
+    });
+  }, []);
+  return (
+    <>
+      <p>Foo: {foo as any}</p>
+      <button onClick={() => store.set("foo", "Fee Fye Fo Thumb")}>Foo</button>
+    </>
+  );
+};
+export const Vanilla = VanillaTemplate.bind({});
+VanillaTemplate.args = {};
